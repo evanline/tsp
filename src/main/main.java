2077ab18a5e4dff55ce0b21d21f686ae51ed1ef0
Sanne Klaassen <sanne.geesje@gmail.com>
2017-05-03 11:22:21 +0200

    main gemaakt. test voor github

---
+++ src/main/main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package main;
+
+/**
+ * @author sanne
+ *
+ */
+public class main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
