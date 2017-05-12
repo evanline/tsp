ff7e06563337f5e0bdd779869242b6a1cff98fc0
Ian Hildebrand <ian.hildebrand@hotmail.com>
2017-05-12 16:15:00 +0200

    We Fucked UP fix

--- src/main/Scherm.java
+++ src/main/Scherm.java
@@ -8,11 +8,15 @@ import java.awt.FlowLayout;
 import java.awt.TextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
 import javax.swing.*;
+
+import static java.awt.Color.DARK_GRAY;
 
 /**
  * @author sanne
@@ -63,14 +67,8 @@ public class Scherm extends JFrame implements ActionListener {
 	}
 
 	private ArrayList<Algoritmenenum> algoritmenArrayList = new ArrayList<>();
-	List<Double> pathlengthes1 = new ArrayList<>();
-	List<Double> pathlengthes2 = new ArrayList<>();
-	List<Double> pathlengthes3 = new ArrayList<>();
-	List<Double> pathlengthes4 = new ArrayList<>();
-	List<Double> tijden1 = new ArrayList<>();
-	List<Double> tijden2 = new ArrayList<>();
-	List<Double> tijden3 = new ArrayList<>();
-	List<Double> tijden4 = new ArrayList<>();
+	List<Double> pathlengthes = new ArrayList<>();
+	List<Double> tijden = new ArrayList<>();
 
 	Graph padlengteGraph;
 	Graph berekentijdGraph;
@@ -305,99 +303,63 @@ public class Scherm extends JFrame implements ActionListener {
 
 					if (bruteForceCKBX.getState()) {
 						algoritmenArrayList.add(Algoritmenenum.BRUTEFORCE);
-						/*
-						for (int i = 0; i < aantalSimulaties + 1; i++) {
-							if (!(i == 0)) {
-								BruteForce j = new BruteForce();
-
-								pathlengthes1.add(j.getTotalDistance());
-								tijden1.add(j.getRunTime());
-							} else {
-								pathlengthes1.add(0.0);
-								tijden1.add(0.0);
-							}
-						}
-						 */
-					}
-					Boolean geenalgoritmen = true;
-					for (Checkbox c : algoritmen) {
-						if (c.getState()) {
-							geenalgoritmen = false;
-							System.out.println("checkpoint geen algoritmen leeg");
-						}
 					}
 					if (twoOptCKBX.getState()) {
-						for (int i = 0; i < aantalSimulaties + 1; i++) {
-							if (!(i == 0)) {
-								TwoOpt j = new TwoOpt();
-
-								pathlengthes2.add(j.getTotalDistance());
-								tijden2.add(j.getRunTime());
-							} else {
-								pathlengthes2.add(0.0);
-								tijden2.add(0.0);
-							}
-						}
+						algoritmenArrayList.add(Algoritmenenum.TWOOPT);
 					}
 					if (nearestNeighborCKBX.getState()) {
-						for (int i = 0; i < aantalSimulaties + 1; i++) {
-							if (!(i == 0)) {
-								NearestNeighbor j = new NearestNeighbor();
-
-								pathlengthes3.add(j.getTotalDistance());
-								tijden3.add(j.getRunTime());
-							} else {
-								pathlengthes3.add(0.0);
-								tijden3.add(0.0);
-							}
-						}
-						System.out.println("checkpoint");
+						algoritmenArrayList.add(Algoritmenenum.NEARESTNEIGHBOR);
 					}
 					if (eigenAlgoritmeCKBX.getState()) {
 						algoritmenArrayList.add(Algoritmenenum.EIGENALG);
-						/*
-						for (int i = 0; i < aantalSimulaties + 1; i++) {
-							if (!(i == 0)) {
-								EigenAlgoritme j = new EigenAlgoritme();
-
-								pathlengthes4.add(j.getTotalDistance());
-								tijden4.add(j.getRunTime());
-							} else {
-								pathlengthes4.add(0.0);
-								tijden4.add(0.0);
-							}
-						}
-						 */
 					}
-					if (!geenalgoritmen) {
-						for (Checkbox c : algoritmen) {
-							c.setEnabled(false);
-						}
-						aantalSimulatiesTXT.setEnabled(false);
-						aantaArtikelenlTXT.setEnabled(false);
-						grafiekPNL.removeAll();
 
-
-						Graph padlengteGraph = new Graph(pathlengthes1, pathlengthes2, pathlengthes3, pathlengthes4);
-						padgrafiekPNL.add(padlengte);
-						padgrafiekPNL.add(padlengteGraph, BorderLayout.EAST);
-
-						grafiekPNL.add(padgrafiekPNL, BorderLayout.WEST);
-//		SwingUtilities.invokeLater(() -> Graph.createAndShowGui(pathlengthes1));
-						Graph berekentijdGraph = new Graph(tijden1, tijden2, tijden3, tijden4);
-						tijdgrafiekPNL.add(berekentijd);
-						tijdgrafiekPNL.add(berekentijdGraph, BorderLayout.EAST);
-						grafiekPNL.add(tijdgrafiekPNL, BorderLayout.EAST);
-
-
-						getContentPane().remove(nogEenPanelWantDatWasWatIkNodigHadInMijnLevenPNL);
-						getContentPane().revalidate();
-						getContentPane().repaint();
-
-						getContentPane().add(nogEenPanelWantDatWasWatIkNodigHadInMijnLevenPNL);
+					for (Checkbox c : algoritmen) {
+						c.setEnabled(false);
 					}
-					//	} else if (e.getSource() == pauzeBTN) {
-					//		System.out.println("pauze");
+					aantalSimulatiesTXT.setEnabled(false);
+					aantaArtikelenlTXT.setEnabled(false);
+					grafiekPNL.removeAll();
+
+
+
+					getContentPane().remove(nogEenPanelWantDatWasWatIkNodigHadInMijnLevenPNL);
+					getContentPane().revalidate();
+					getContentPane().repaint();
+					getContentPane().add(nogEenPanelWantDatWasWatIkNodigHadInMijnLevenPNL);
+
+					for (int i = 0; i < aantalSimulaties + 1; i++) {
+						if (!(i == 0)) {
+							NearestNeighbor j = new NearestNeighbor();
+
+							pathlengthes.add(j.getTotalDistance());
+							tijden.add(j.getRunTime());
+						} else {
+							pathlengthes.add(0.0);
+							tijden.add(0.0);
+						}
+					}
+					grafiekPNL.removeAll();
+					Graph padlengteGraph = new Graph(pathlengthes);
+					padgrafiekPNL.add(padlengte);
+					padgrafiekPNL.add(padlengteGraph, BorderLayout.EAST);
+
+					grafiekPNL.add(padgrafiekPNL, BorderLayout.WEST);
+//		SwingUtilities.invokeLater(() -> Graph.createAndShowGui(pathlengthes));
+					Graph berekentijdGraph = new Graph(tijden);
+					tijdgrafiekPNL.add(berekentijd);
+					tijdgrafiekPNL.add(berekentijdGraph, BorderLayout.EAST);
+					grafiekPNL.add(tijdgrafiekPNL, BorderLayout.EAST);
+
+
+					getContentPane().remove(nogEenPanelWantDatWasWatIkNodigHadInMijnLevenPNL);
+					getContentPane().revalidate();
+					getContentPane().repaint();
+
+					getContentPane().add(nogEenPanelWantDatWasWatIkNodigHadInMijnLevenPNL);
+
+			//	} else if (e.getSource() == pauzeBTN) {
+			//		System.out.println("pauze");
 				} else if (e.getSource() == resetBTN) {
 					System.out.println("reset");
 					for (Checkbox c : algoritmen) {
