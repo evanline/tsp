37f3c5ec5a6bb5130f83539cc2be6c210672760f
Ian Hildebrand <ian.hildebrand@hotmail.com>
2017-05-12 13:44:33 +0200

    stuff

--- src/main/AlgorithmInterface.java
+++ src/main/AlgorithmInterface.java
@@ -1,11 +1,10 @@
 package main;
-
 /**
  * Created by: Ian Hildebrand
  * Date: 10-May-17.
  */
-public interface AlgorithmInterface {
-
+public interface AlgorithmInterface
+{
 	/**
 	 * get the total distance of the path calculated.
 	 * @return total distance in squares.
