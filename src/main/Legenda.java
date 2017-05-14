543b001b72e4addff2cf78d9229cd584fd0ca49f
Sanne Klaassen <sanne.geesje@gmail.com>
2017-05-14 14:00:29 +0200

    legenda werkt!

--- src/main/Legenda.java
+++ src/main/Legenda.java
@@ -16,11 +16,11 @@ public class Legenda extends JPanel {
 	private Checkbox cbx4;
 	private static final int PREF_W = 800; //breedte tenzij anders nodig
 	private static final int PREF_H = 70; //hoogte tenzij anders nodig
-	
+
 	private List<Color> kleuren = new ArrayList<>();
 
 
-	public Legenda(Checkbox cbx1, Checkbox cbx2, Checkbox cbx3, Checkbox cbx4) {
+	Legenda(Checkbox cbx1, Checkbox cbx2, Checkbox cbx3, Checkbox cbx4) {
 		this.cbx1 = cbx1;
 		this.cbx2 = cbx2;
 		this.cbx3 = cbx3;
@@ -32,47 +32,44 @@ public class Legenda extends JPanel {
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		List<String> namen = new ArrayList<>();
-		if(cbx1.getState()){
+		if (cbx1.getState()) {
 			String naam1 = "Brute Force";
 			Color graphcolor1 = Graph.getGraphColor1();
 			namen.add(naam1);
 			kleuren.add(graphcolor1);
 		}
-		if(cbx2.getState()){
+		if (cbx2.getState()) {
 			String naam2 = "2-Opt";
 			Color graphcolor2 = Graph.getGraphColor2();
 			namen.add(naam2);
 			kleuren.add(graphcolor2);
 		}
-		if(cbx3.getState()){
+		if (cbx3.getState()) {
 			String naam3 = "Nearest Neighbor";
 			Color graphcolor3 = Graph.getGraphColor3();
-			namen.add("hoi?");
+			namen.add(naam3);
 			kleuren.add(graphcolor3);
 		}
-		if(cbx4.getState()){
+		if (cbx4.getState()) {
 			String naam4 = "Eigen Algoritme";
 			Color graphcolor4 = Graph.getGraphColor4();
 			namen.add(naam4);
 			kleuren.add(graphcolor4);
 		}
 
-		for (int i = 0; i < namen.size(); i++){
+		for (int i = 0; i < namen.size(); i++) {
 			g.setColor(Color.BLACK);
-			g.drawString(namen.get(i), 0, 30); //TODO: de coordinaten kloppen voor geen pixel...
+			int x0 = i * 150 + 25;
+			g.drawString(namen.get(i), x0, 40);
 
 			g.setColor(kleuren.get(i));
-			int x = 90;
-			if (i > 2){
-				x = 0;
-			}
-			int y = 30;
-			if (i%2 == 0){
-				y = 60;
-			}
-			g.fillRect(x,y,20,20);
+			int x = i * 150;
+			int y = 25;
+			g.fillRect(x, y, 20, 20);
+
 		}
 	}
+
 	@Override
 	public Dimension getPreferredSize() {
 		return new Dimension(PREF_W, PREF_H);
