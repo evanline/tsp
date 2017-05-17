8f9b54f3ca794dc1efa7c267eb92e5878942acd4
Ian Hildebrand <ian.hildebrand@hotmail.com>
2017-05-17 11:48:16 +0200

    TwoOpt

--- src/main/TwoOpt.java
+++ src/main/TwoOpt.java
@@ -17,16 +17,35 @@ public class TwoOpt implements AlgorithmInterface
 		long startTime = System.nanoTime();
 		Integer[] currentPos = new Integer[]{0, 0};
 
-		for (Integer[] i : list)
+		for (int i = 0; i < list.size(); i++)
 		{
-			double dist = Math.sqrt((Math.pow((Math.abs(i[0] - currentPos[0]) ), 2) + Math.pow(Math.abs(i[1] - currentPos[1]), 2))); // √((|x - y|)² + (|a - i|)²)
-			path.add(i);
-			totalDistance += dist;
-			currentPos = i;
+			i = ((i + 3) > list.size()) ? list.size() : (i + 3);
+
+			Integer[] x = list.get(i - 3);
+			Integer[] y = list.get(i - 2);
+			Integer[] u = list.get(i - 1);
+			Integer[] v = list.get(i);
+
+			double route1 = calculateDistance(x, y);
+			double route2 = calculateDistance(u, v);
+
+			double costR1 = route1 + route2;
+
+			double route3 = calculateDistance(x, u);
+			double route4 = calculateDistance(y, v);
+
+			double costR2 = route3 + route4;
+
+
 		}
 
 		long endTime = System.nanoTime();
 		timeSpend = (endTime - startTime) / (1 * Math.pow(10, 6));
+	}
+
+	private double calculateDistance(Integer[] a,Integer[] b)
+	{
+		return Math.sqrt((Math.pow((Math.abs(b[0] - a[0]) ), 2) + Math.pow(Math.abs(b[1] - a[1]), 2)));
 	}
 
 	public ArrayList<Integer[]> getPath()
