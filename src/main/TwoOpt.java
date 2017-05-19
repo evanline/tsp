53685ba9aba3585056aca15cef49bf40039874e1
Ian Hildebrand <ian.hildebrand@hotmail.com>
2017-05-19 14:35:01 +0200

    nigga what

--- src/main/TwoOpt.java
+++ src/main/TwoOpt.java
@@ -6,8 +6,8 @@ import java.util.Arrays;
 import java.util.Collections;
 
 /**
- * Created by: Sanne Klaassen
- * Date: 18/05/17.
+ * Created by: Ian Hildebrand
+ * Date: 19-05-17.
  */
 public class TwoOpt implements AlgorithmInterface {
 	private ArrayList<Integer[]> path = new ArrayList<>();
@@ -22,8 +22,9 @@ public class TwoOpt implements AlgorithmInterface {
 
 		list = new NearestNeighbor(list).getPath();
 
-		while (this.previousTotalDistance >= this.totalDistance || previousTotalDistance == -1d)
+		while (true)
 		{
+			if (!(previousTotalDistance > totalDistance || previousTotalDistance == -1d)) break;
 			run2Opt(list);
 		}
 
@@ -41,11 +42,7 @@ public class TwoOpt implements AlgorithmInterface {
 		for (Integer[] a : list)
 		{
 			int indexa = list.indexOf(a);
-			if ((list.size()-1) < (indexa+1))
-			{
-				continue;
-			}
-
+			if ((list.size()-1) < (indexa+1)) continue;
 			Integer[] b = list.get(indexa+1);
 
 			for(Integer[] c : list)
@@ -54,32 +51,14 @@ public class TwoOpt implements AlgorithmInterface {
 				if ((list.size()-1) < (indexc+1)) continue;
 				Integer[] d = list.get(indexc+1);
 				if (c == a || c == b || d == a || d == b) continue;
-
-				Line2D line1 = new Line2D.Float(a[0], a[1], b[0], b[1]);
-				Line2D line2 = new Line2D.Float(c[0], c[1], d[0], d[1]);
-
-				if (line2.intersectsLine(line1))
-				{
-					double path1 = calculateDistance(a, b);
-					double path2 = calculateDistance(c, d);
-					double path3 = calculateDistance(a, c);
-					double path4 = calculateDistance(b, d);
-
-					double route1 = path1 + path2;
-					double route2 = path3 + path4;
-
-					if (route1 > route2)
-					{
-						Collections.swap(list, indexa+1, indexc);
-					}
-				}
+				if (new Line2D.Float(c[0], c[1], d[0], d[1]).intersectsLine(new Line2D.Float(a[0], a[1], b[0], b[1])))
+					if ((calculateDistance(a, b) + calculateDistance(c, d)) > (calculateDistance(a, c) + calculateDistance(b, d)))
+						Collections.swap(list, indexa + 1, indexc);
 			}
 		}
 		calculateTotalDistance(list);
 		if((previousTotalDistance > totalDistance) || previousTotalDistance == -1d)
-		{
 			this.path = list;
-		}
 	}
 
 	private void calculateTotalDistance(ArrayList<Integer[]> list)
