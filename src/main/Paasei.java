c76cc6ab226b33d1cf9332f295dd456a0f179572
Sanne Klaassen <sanne.geesje@gmail.com>
2017-05-24 14:58:29 +0200

    iaaaannnn

--- src/main/Paasei.java
+++ src/main/Paasei.java
@@ -1,5 +1,8 @@
 package main;
 
+import chrriis.dj.nativeswing.swtimpl.NativeInterface;
+import chrriis.dj.nativeswing.swtimpl.components.JWebBrowser;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -7,23 +10,16 @@ import java.awt.*;
  * Created by: Sanne Klaassen
  * Date: 24/05/17.
  */
-class Paasei extends JFrame{
-	Paasei(){
-		setTitle("Easter Egg");
-		setSize(800,800);
-		setLayout(new FlowLayout());
-		getContentPane().setBackground(Color.gray);
-		setVisible(true);
-		ativeInterface.open();
-		SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				JFrame frame = new JFrame("YouTube Viewer");
-				frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-				frame.getContentPane().add(getBrowserPanel(), BorderLayout.CENTER);
-				frame.setSize(800, 600);
-				frame.setLocationByPlatform(true);
-				frame.setVisible(true);
-			}
+class Paasei extends JFrame {
+	Paasei() {
+		NativeInterface.open();
+		SwingUtilities.invokeLater(() -> {
+			JFrame frame = new JFrame("YouTube Viewer");
+			frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+			frame.getContentPane().add(getBrowserPanel(), BorderLayout.CENTER);
+			frame.setSize(800, 600);
+			frame.setLocationByPlatform(true);
+			frame.setVisible(true);
 		});
 		NativeInterface.runEventPump();
 		// don't forget to properly close native components
@@ -35,14 +31,15 @@ class Paasei extends JFrame{
 		}));
 	}
 
-	public static JPanel getBrowserPanel() {
-		JPanel webBrowserPanel = new JPanel(new BorderLayout());
-		JWebBrowser webBrowser = new JWebBrowser();
-		webBrowserPanel.add(webBrowser, BorderLayout.CENTER);
-		webBrowser.setBarsVisible(false);
-		webBrowser.navigate("https://www.youtube.com/v/b-Cr0EWwaTk?fs=1");
-		return webBrowserPanel;
-	}
+		private static JPanel getBrowserPanel() {
+			JPanel webBrowserPanel = new JPanel(new BorderLayout());
+			JWebBrowser webBrowser = new JWebBrowser();
+			webBrowserPanel.add(webBrowser, BorderLayout.CENTER);
+			webBrowser.setBarsVisible(false);
+			webBrowser.navigate("https://www.youtube.com/v/b-Cr0EWwaTk?fs=1");
+			return webBrowserPanel;
+		}
 
-	}
+
 }
+
