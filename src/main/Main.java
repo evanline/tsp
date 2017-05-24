0f69bd40767b2a741cc54e2b38cf8e7719b1037d
Ian Hildebrand <ian.hildebrand@hotmail.com>
2017-05-24 14:21:24 +0200

    beep beep ima sheep

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,12 @@
 package main;
 
+import javafx.embed.swing.JFXPanel;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
 import javax.swing.*;
+import java.io.File;
+
 
 /**
  * @author Sanne Klaassen & Ian Hildebrand
@@ -10,5 +16,21 @@ public class Main {
 	public static void main(String[] args)
 	{
 		new Scherm().setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		new JFXPanel();
+
+//		new Thread(() ->
+//		{
+//			try
+//			{
+//				String bip = "src/main/meep.mp3";
+//				Media hit = new Media(new File(bip).toURI().toString());
+//				MediaPlayer mediaPlayer = new MediaPlayer(hit);
+//				mediaPlayer.play();
+//			}
+//			catch (Exception e)
+//			{
+//				e.printStackTrace();
+//			}
+//		}).start();
 	}
 }
