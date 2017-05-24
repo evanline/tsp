0ab67bdd3f9e7bbd90217b9fd58243a06fc3a5f6
Sanne Klaassen <sanne.geesje@gmail.com>
2017-05-24 14:05:54 +0200

    nieuw scherm

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,8 @@
 package main;
 
 import javax.swing.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 /**
  * @author Sanne Klaassen & Ian Hildebrand
@@ -9,6 +11,19 @@ import javax.swing.*;
 public class Main {
 	public static void main(String[] args)
 	{
-		new Scherm().setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		Scherm hoofdscherm = new Scherm();
+		hoofdscherm.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+		hoofdscherm.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent evt) {
+				hoofdscherm.setVisible(false);
+				System.out.println("test 1");
+				new Paasei().setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+			}
+		});
+	/*	if (!hoofdscherm.isVisible()){
+			System.out.println("test 2");
+
+		}*/
 	}
 }
---
+++ src/main/Paasei.java
@@ -0,0 +1,20 @@
+package main;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by: Sanne Klaassen
+ * Date: 24/05/17.
+ */
+class Paasei extends JFrame{
+	Paasei(){
+		setTitle("Easter Egg");
+		setSize(800,800);
+		setLayout(new FlowLayout());
+		getContentPane().setBackground(Color.gray);
+		setVisible(true);
+
+
+	}
+}
